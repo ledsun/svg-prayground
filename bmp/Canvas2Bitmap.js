8eb922b25b75c2c02d18ebcf479a19d896a32efb
shigeru.nakajima <shigeru.nakajima@gmail.com>
2020-08-10 08:02:41 +0900

    24ビットビットマップフォーマットのパディング計算を間違えていました

--- bmp/Canvas2Bitmap.js
+++ bmp/Canvas2Bitmap.js
@@ -23,11 +23,11 @@ module.exports = class Canvas2Bitmap {
   // 行単位のパディングを求める
   // 1行のデータが4バイトの倍数出なかった場合は、4バイトになるまで埋めます。
   get _linePadding() {
-    return (this._width * this._depth) % 4;
+    return 4 - (this._width * this._depth) % 4;
   }
 
   get _lineDataSize() {
-    return (this._width + this._linePadding) * this._depth
+    return this._width * this._depth + this._linePadding
   }
 
   get _bodySize() {
